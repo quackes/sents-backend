bc989dc5c6b8a38b3fd47486be84e9ab7ada1cba
Tobias Wingerter <tobias.wingerter@provinzial.com>
2018-11-21 17:20:40 +0100

    in memory storage

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 let express = require('express'),
-    expressWs = require('express-ws')
+    expressWs = require('express-ws'),
+    bodyParser = require('body-parser')
     morgan = require('morgan');
 
 let app = express()
 
+app.use(bodyParser.json())
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 9000,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+let data = null;
 
 app.get('/',(req, res)=>{
     res.send('works')
-})
+});
+
+app.put('/storage', (req, res) => {
+    data =  req.body
+    res.send(201);
+});
+
+app.get('/storage', (req, res) => {
+    res.send(data)
+});
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
